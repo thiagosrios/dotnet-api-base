4e6075a221db327b9e7268a404ea7679ea4eb5b7
Thiago de Souza Rios <thiagosrios@gmail.com>
2020-04-29 18:29:40 -0300

    Adicionando validação do model

--- src/Database/Context.cs
+++ src/Database/Context.cs
@@ -20,8 +20,10 @@ namespace ApiBase.Database
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<User>()
-                .HasKey(e => e.Id);
+            builder.Entity<User>().HasKey(e => e.Id);
+            builder.Entity<User>().HasIndex(e => e.Login).IsUnique();
+            builder.Entity<User>().HasIndex(e => e.Email).IsUnique();
+            builder.Entity<User>().HasIndex(e => new { e.FirstName, e.LastName }).IsUnique();
 
             base.OnModelCreating(builder);
         }
--- src/Models/User.cs
+++ src/Models/User.cs
@@ -8,10 +8,21 @@ namespace ApiBase.Models
         [Key]
         [JsonIgnore]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "O sobrenome é obrigatório")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "O endereço fornecido é inválido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O login é obrigatório")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
         public string Password { get; set; }
     }
 }
